fcfd57689ae0efeb6169dd73ac4a0a0e0ff5c4b9
rferro-d <rapha4.lx@gmail.com>
2024-09-24 19:15:09 -0300

    Start create test for ft_memset

--- tests.c
+++ tests.c
@@ -1,11 +1,37 @@
 #include <criterion/criterion.h>
 
-Test(math, addition) {
-    int result = 5 + 3;
-    cr_assert(result == 8, "Expected 5 + 3 to be 8, but got %d", result);
+#include "./lib/libft.h"
+
+
+bool array_are_equal(int *arr1, int* arr2, size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+		if (arr1[i] != arr2[i])
+			return (0);
+	return (1);
 }
 
-Test(math, subtraction) {
-    int result = 5 - 3;
-    cr_assert(result == 2, "Expected 5 - 3 to be 2, but got %d", result);
+Test(memset, string_basic){
+        char ft_test[] = "dddd";
+	char mem_test[] = "dddd";
+
+        ft_memset(ft_test, 'a', 2);
+	memset(mem_test, 'a', 2);
+	cr_assert_str_eq(ft_test, mem_test, "returned: %s, but got %s", mem_test, ft_test);
 }
+
+Test(memset, int_array_basic){
+        int ft_test[] = {0,1,2,3,4,5,6};
+        int origin[] = {0,1,2,3,4,5,6};
+
+        ft_memset(ft_test, 1802, 2);
+        memset(origin, 1802, 2);
+
+	cr_assert(array_are_equal(ft_test, origin, 7));
+}
+
+
+
+
+
+
